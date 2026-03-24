ee3ac508927bdc6515b7a77f5d6618057a09db1d
vyckPFH <victoria.c.oliveira08@gmail.com>
2026-03-24 11:59:21 -0300

    a

--- maisHeracaEMaisInterface/lib/IngressoVIP.java
+++ maisHeracaEMaisInterface/lib/IngressoVIP.java
@@ -1,15 +1,19 @@
 public class IngressoVIP extends Ingresso implements VIP {
     private double taxa;
-    @Override
-    public double getTaxaVip() {
-
-        throw new UnsupportedOperationException("Unimplemented method 'getTaxaVip'");
-    }
+    
 
     @Override
     public double calcValorCmTaxa() {
 
         throw new UnsupportedOperationException("Unimplemented method 'calcValorCmTaxa'");
+    }
+
+
+    @Override
+    public double showTaxa() {
+        // TODO Auto-generated method stub
+        
+        throw new UnsupportedOperationException("Unimplemented method 'showTaxa'");
     }
     
 }
